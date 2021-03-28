0c01e74c17bb604ce725785528d0b6612c0169d5
Galatea <marcinevskaya93@mail.ru>
2021-03-28 16:56:07 +0300

    Initial Commit

---
+++ UI Kit App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UI Kit App
+//
+//  Created by Larisa on 28.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
